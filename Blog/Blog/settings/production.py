da532d25b901107ba97e4663a0b6ec9ca65c87e3
Loreisis Hernandez <39110420+Loreisiss@users.noreply.github.com>
2020-10-17 19:47:43 -0400

    'credenciales'

--- Blog/Blog/settings/production.py
+++ Blog/Blog/settings/production.py
@@ -1,4 +1,4 @@
-from .base import*
+from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -8,7 +8,11 @@ ALLOWED_HOSTS = ['djangoblogcodigopy.herokuapp.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':  'd9food3c84etq4',
+       'USER':  'qcmbcnwvyllvpo',
+        'PASSWORD':  '45c1a14c2b08492a88e8a61738c9b7f3336ce472eb0f71aef3d0aa3c621a832f',
+        'HOST':'ec2-23-23-242-234.compute-1.amazonaws.com',
+        'PORT':5432,
     }
 }
